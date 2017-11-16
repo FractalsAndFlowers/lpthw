d4913a7e43a0e5ce87f4c88975c3592f0dff0c17
BellaKova <isabella.kovarik@gmail.com>
2017-11-16 16:44:23 -0500

    Example 16: Reading and Writing Files

--- ex15/ex15.py
+++ ex15/ex15.py
@@ -7,7 +7,16 @@ txt = open(filename)
 print(f"Here's your file {filename}:")
 print(txt.read())
 
+print("Type the filename again:")
+file_again = input("> ")
 
+txt_again = open(file_again)
+
+print(txt_again.read())
+
+# not sure if this actually works.
+txt.close()
+txt_again.close()
 
 # Lines 1-3 use argv to get a filename.'
 # Line 5 introduces the command  open. It takes a parameter and returns a value you can set to your own variable.
